e0f250ce67372ae14a0a274dcb2bb6b016dac535
Gero Konberg <gerokonberg@proton.me>
2026-05-13 22:59:43 +0200

    Add files via upload

--- presets/settings-RuinArm.asm
+++ presets/settings-RuinArm.asm
@@ -1,10 +1,10 @@
 ;konvertMML settings
 !ReadFile = "input.spc"
 
-!ReadAddr = $3A00
+!ReadAddr = $1E00
 !OutAddr = $7000
 !ProgAddr = $F000
 
 !ReadIndex = $01
 
-incsrc "asm/readVSt2.asm"
+incsrc "asm/readV120.asm"
